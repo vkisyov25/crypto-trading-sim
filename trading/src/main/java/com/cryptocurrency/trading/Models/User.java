cfed3ab68b417f7751b61d610a99add58516648a
vkisyov25 <ventsislav755@gmail.com>
2025-04-03 22:02:18 +0300

    updated User class

--- trading/src/main/java/com/cryptocurrency/trading/Models/User.java
+++ trading/src/main/java/com/cryptocurrency/trading/Models/User.java
@@ -15,4 +15,37 @@ public class User {
     private int id;
     private String username;
     private BigDecimal balance;
+
+    public User(int id, String username, BigDecimal balance) {
+        this.id = id;
+        this.username = username;
+        this.balance = balance;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
