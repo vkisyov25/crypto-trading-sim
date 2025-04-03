34cdefab1703fb7129e0f79b50786d76baa2bcc7
vkisyov25 <ventsislav755@gmail.com>
2025-04-03 21:57:51 +0300

    created UserService

---
+++ trading/src/main/java/com/cryptocurrency/trading/Service/UserService.java
@@ -0,0 +1,27 @@
+package com.cryptocurrency.trading.Service;
+
+import com.cryptocurrency.trading.DAO.UserDao;
+import com.cryptocurrency.trading.Models.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
+@Service
+public class UserService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void createUser(User user) throws SQLException {
+        if (userDao.userExists(user.getUsername())) {
+            throw new IllegalArgumentException("Потребител с това име вече съществува!");
+        }
+        userDao.create(user);
+    }
+}
