2275b5ab08e55825f03a2cfad568c08ae2ff6f63
vkisyov25 <ventsislav755@gmail.com>
2025-04-03 21:58:48 +0300

    created the method create() and userExists

---
+++ trading/src/main/java/com/cryptocurrency/trading/DAO/UserDao.java
@@ -0,0 +1,43 @@
+package com.cryptocurrency.trading.DAO;
+
+import com.cryptocurrency.trading.Models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+public class UserDao {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public void create(User user) throws SQLException {
+        String sql = "INSERT INTO users (username, balance) VALUES (?, ?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setBigDecimal(2, user.getBalance());
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public boolean userExists(String username) throws SQLException {
+        String sql = "SELECT COUNT(*) FROM users WHERE username = ?";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1) > 0;
+            }
+        }
+        return false;
+    }
+
+
+}
