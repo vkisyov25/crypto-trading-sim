f260c2f67573f86617b5bfce8a4757ea68a50cc6
vkisyov25 <ventsislav755@gmail.com>
2025-04-07 11:52:10 +0300

    created a calculateBuySummary() method which return BuySummaryDto

--- trading/src/main/java/com/cryptocurrency/trading/DAO/TransactionDao.java
+++ trading/src/main/java/com/cryptocurrency/trading/DAO/TransactionDao.java
@@ -1,5 +1,6 @@
 package com.cryptocurrency.trading.DAO;
 
+import com.cryptocurrency.trading.Dtos.BuySummaryDto;
 import com.cryptocurrency.trading.Models.Transaction;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -7,27 +8,44 @@ import org.springframework.stereotype.Repository;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Repository
 @RequiredArgsConstructor
 public class TransactionDao {
     private final DataSource dataSource;
+
     public void save(Transaction transaction) throws SQLException {
         String sql = "INSERT INTO transactions (symbol,type,quantity, price, profit_loss, date, user_id) VALUES (?,?,?,?,?,?,?)";
 
-        try(Connection connection = dataSource.getConnection();
-            PreparedStatement preparedStatement= connection.prepareStatement(sql)){
-            preparedStatement.setString(1,transaction.getSymbol());
-            preparedStatement.setString(2,transaction.getType().toString());
-            preparedStatement.setBigDecimal(3,transaction.getQuantity());
-            preparedStatement.setBigDecimal(4,transaction.getPrice());
-            preparedStatement.setBigDecimal(5,transaction.getProfitLoss());
+        try (Connection connection = dataSource.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, transaction.getSymbol());
+            preparedStatement.setString(2, transaction.getType().toString());
+            preparedStatement.setBigDecimal(3, transaction.getQuantity());
+            preparedStatement.setBigDecimal(4, transaction.getPrice());
+            preparedStatement.setBigDecimal(5, transaction.getProfitLoss());
             preparedStatement.setTimestamp(6, new java.sql.Timestamp(transaction.getLocalDateTime().getTime()));
-            preparedStatement.setInt(7,transaction.getUserId());
+            preparedStatement.setInt(7, transaction.getUserId());
 
             preparedStatement.executeUpdate();
 
         }
+    }
+
+    public BuySummaryDto calculateBuySummary(int userId, String symbol) throws SQLException {
+        String sql = "SELECT SUM(quantity) AS total_quantity, SUM(quantity * price) AS total_price FROM transactions WHERE user_id = ? AND symbol = ? AND type = 'BUY'";
+
+        BuySummaryDto buySummaryDto = new BuySummaryDto();
+        try (Connection connection = dataSource.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, userId);
+            preparedStatement.setString(2, symbol);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                buySummaryDto.setTotalQuantity(resultSet.getBigDecimal("total_quantity"));
+                buySummaryDto.setTotalPrice(resultSet.getBigDecimal("total_price"));
+            }
+        }
+        return buySummaryDto;
     }
 }
